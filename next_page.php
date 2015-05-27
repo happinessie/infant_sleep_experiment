7d9c0c47f66b6d13bf7a3cb97f66a6a714a64f08
nessie <vanessa.radlinger@gmail.com>
2015-05-27 16:06:03 -0500

    updated new_page

--- next_page.php
+++ next_page.php
@@ -3,7 +3,7 @@
 	$servername = "localhost";
 	$username = "webadmin";
 	$password = "ieeecs";
-	$dbname = "ieeecs";
+	$dbname = "infant_sleep";
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	// Check connection
@@ -19,71 +19,79 @@
 		$phase = $row["exp_phase"];
 		$condition = $row["exp_condition"];
 		$iteration = $row["exp_iteration"];
-	}
-	$new_phase = NULL;
-	$new_condition = $condition;
-	$new_iteration = NULL;
-	$result_URL = NULL;
-	
-	switch ($phase):
-		case "entry":
-			$new_phase = "baseline_pre";
-			$result_URL = "create.html";
-			$new_condition = rand(1,3);
-			break;
-		case "baseline_pre":
-			$new_phase = "generalization_pre";
-			$result_URL = "assess.html";
-			break;
-		case "instruction":
-			if ($condition==1) {
+		
+		
+		$new_phase = $phase;
+		$new_condition = $condition;
+		$new_iteration = $iteration;
+		$result_URL = NULL;
+		
+		switch ($phase):
+			case "entry":
+				$new_phase = "baseline_pre";
+				$result_URL = "create.html";
+				$new_condition = rand(1,3);
+				break;
+			case "baseline_pre":
+				$new_phase = "generalization_pre";
+				$result_URL = "assess.html";
+				break;
+			case "instruction":
+				if ($condition==1) {
+					$new_phase = "baseline";
+					$result_URL = "create.html";
+				}
+				if ($condition==2 || $condition==3) {
+					$new_phase = "modelling";
+					$result_URL = "good_example.html";
+				}
+				if ($condition < 1 || $condition > 3) {
+					$result_URL = "ERRORL Invalid condition!";
+				}
+				break;
+			case "modelling":
+				$new_phase = "rehearsal-feedback";
+				$result_URL = "create.html?";
+				break;
+			case "rehearsal-feedback":
 				$new_phase = "baseline";
 				$result_URL = "create.html";
-			}
-			if ($condition==2 || $condition==3) {
-				$new_phase = "modelling";
-				$result_URL = "good_example.html";
-			}
-			if ($condition < 1 || $condition > 3) {
-				$result_URL = "ERRORL Invalid condition!";
-			}
-			break;
-		case "modelling":
-			$new_phase = "rehearsal-feedback";
-			$result_URL = "create.html?";
-			break;
-		case "rehearsal-feedback":
-			$new_phase = "baseline";
-			$result_URL = "create.html";
-			break;
-		case "baseline":
-			if ($condition==1 || $condition==2) {
-				$new_phase = "generalization_post";
-				$result_URL = "assess.html";
-			}
-			if ($condition==3) {
-				if ($_GET["pass"] == true){
+				break;
+			case "baseline":
+				if ($condition==1 || $condition==2) {
 					$new_phase = "generalization_post";
 					$result_URL = "assess.html";
 				}
-				else{
-					$new_phase = "instruction";
-					$result_URL = "guidelines.html";
-					$new_iteration = $iteration + 1;
+				if ($condition==3) {
+					if ($_GET["pass"] == true){
+						$new_phase = "generalization_post";
+						$result_URL = "assess.html";
+					}
+					else{
+						$new_phase = "instruction";
+						$result_URL = "guidelines.html";
+						$new_iteration = $iteration + 1;
+					}
 				}
-			}
-			if ($condition < 1 || $condition > 3) {
-				$result_URL = "ERRORL Invalid condition!";
-			}
-			break;
-		case "generalization_post":
-			$new_phase = "exit";
-			$result_URL = "exit.html";
-			break;
-		default:
-			$new_phase = "entry";
-			$result_URL = "entry.html";
-	endswitch;
+				if ($condition < 1 || $condition > 3) {
+					$result_URL = "ERRORL Invalid condition!";
+				}
+				break;
+			case "generalization_post":
+				$new_phase = "exit";
+				$result_URL = "exit.html";
+				break;
+			default:
+				$new_phase = "entry";
+				$result_URL = "entry.html";
+		endswitch;
+		
+		$sql = "UPDATE `Participants` SET exp_condition='".$new_condition."', exp_phase=`".$new_phase."` exp_iteration=".$new_iteration." WHERE participant_id = ".$_GET["participant_id"];
+		$results = mysqli_query($conn,$sql);
 
-	echo($result_URL);
+		echo($result_URL);
+	}
+	else {
+		echo("ERROR: No participant found!");
+	}
 ?>
