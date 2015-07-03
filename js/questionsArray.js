773d53b9feee3bed6606a90716b3f4c26154212c
grin2b <jeremy.f.le.veque@gmail.com>
2015-07-02 19:18:23 -0500

    Changed directory structure a bit, added sample array

---
+++ js/questionsArray.js
@@ -0,0 +1,22 @@
+var questions =
+[
+	{
+		"qText": "What is the answer to this question?",
+		"qShortText": "this question",
+		"nextQ": "ending",
+		"responses":
+		[
+			{
+				"rText": "This is the answer to this question!",
+				"rShortText": "the answer",
+				"rCorrect": true,
+				"nextQ": "ending"
+			},
+			{
+				"rText": "This is not the answer to this question",
+				"rShortText": "not the answer",
+				"rCorrect": false
+			}
+		]
+	}
+];
